ed1d917d3e801d81588c31e94a7f20b80a22fcae
Jeff Laughlin <jeff@jefflaughlinconsulting.com>
2013-04-04 13:13:50 -0700

    monkey patch auto_convert to fix dotted quad issue

--- kudu/__init__.py
+++ kudu/__init__.py
@@ -4,3 +4,20 @@ import os
 
 sys.path.append(os.environ['ANTELOPE'] + '/data/python')
 
+from antelope import stock
+
+
+# Monkeypatch auto_convert to work around it interpreting dotted quads as time
+# strings.
+old_auto_convert = stock.auto_convert
+
+def auto_convert(val):
+    try:
+        if val.count('.') > 1:
+            return val
+    except AttributeError:
+        pass
+    return old_auto_convert(val)
+
+stock.auto_convert = auto_convert
+
