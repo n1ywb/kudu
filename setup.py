4c9034e6372dbb7caa8aacbee9fdb39f7720c290
Jeff Laughlin <jeff@jefflaughlinconsulting.com>
2013-04-04 13:13:31 -0700

    added setup.py so can pip install in ve

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='kudu',
+      version='0.1',
+      description='Kudu: The most delicious antelope',
+      author='Array Network Facility, UCSD',
+      author_email='anf@ucsd.edu',
+      url='http://anf.ucsd.edu',
+      packages=['kudu'],
+     )
+
